ca0abf96761cd4b29699c74b26aaca51fbee3cd6
debrasnicolas <nicodebras17@gmail.com>
2023-04-03 20:34:35 +0200

    complete crud player

--- server_api/urls.py
+++ server_api/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('player/', views.AllPlayerGet),
-    path('player/<int:id>/', views.PlayerGet)
+    path('player/<int:id>/', views.PlayerGet),
+    path('addplayer/', views.PlayerAdd),
+    path('updateplayer/<int:id>/', views.PlayerUpdate),
+    path('deleteplayer/<int:id>/', views.PlayerDelete)
 ]
     
--- server_api/views.py
+++ server_api/views.py
@@ -13,7 +13,42 @@ def AllPlayerGet(request):
 #recupère un seul utilisateur 
 @api_view(['GET'])
 def PlayerGet(request, id):
-    res = Player.objects.get(id=id)
+    try:
+        res = Player.objects.get(id=id)
+    except:
+        return Response(status=404)
     serializers = PlayerSerializers(res)
     return Response(serializers.data)
 
+#Ajout un untilisateur 
+@api_view(['POST'])
+def PlayerAdd(request):
+    serializers = PlayerSerializers(data = request.data)
+    if serializers.is_valid():
+        serializers.save()
+    else:
+        return Response(status=400)
+    return Response(serializers.data)
+
+@api_view(['PUT'])
+def PlayerUpdate(request, id):
+    try:
+        res = Player.objects.get(id=id)
+    except:
+        return Response(status=404)
+    serializers = PlayerSerializers(instance=res,data = request.data)
+    if serializers.is_valid():
+        serializers.save()
+    else:
+        return Response(status=400)
+    return Response(serializers.data)
+
+#delete un player
+@api_view(['DELETE'])
+def PlayerDelete(request, id):
+    try:
+        res = Player.objects.get(id=id)
+    except:
+        return Response(status=404)
+    res.delete()
+    return Response(status=200)
