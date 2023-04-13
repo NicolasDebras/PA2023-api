6c8a4e1391f5c198aacd25089278b982eec237bd
debrasnicolas <nicodebras17@gmail.com>
2023-04-13 11:15:53 +0200

    add test but not work

--- server_api/tests.py
+++ server_api/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.urls import reverse_lazy
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import Player
+
+class TestCategory(APITestCase):
+    
+
+    url = reverse_lazy('player')
+
+    #voir bdd si possible de créer un env de test
+    def test_list_player(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+
+    
