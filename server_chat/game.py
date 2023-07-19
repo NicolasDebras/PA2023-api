8c6a898084f82f3f75987b93799359111202590f
debrasnicolas <nicodebras17@gmail.com>
2023-07-19 17:57:01 +0200

    fix room name

--- server_chat/game.py
+++ server_chat/game.py
@@ -7,7 +7,7 @@ from asgiref.sync import sync_to_async
 class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['party_id']
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.room_group_name = 'game_%s' % self.room_name
 
         # Join room group
         await self.channel_layer.group_add(
